57b336d15c5c64fa364c17b763130802421712de
João Pedro <jonhpitter2004@hotmail.com>
2025-07-04 09:02:34 -0300

    cors

--- api-gateway/src/main/java/com/controlegastos/apigateway/config/SecurityConfig.java
+++ api-gateway/src/main/java/com/controlegastos/apigateway/config/SecurityConfig.java
@@ -5,6 +5,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @Configuration
 @EnableWebFluxSecurity
@@ -19,5 +24,19 @@ public class SecurityConfig {
                         .anyExchange().permitAll()
                 )
                 .build();
+    }
+
+    @Bean
+    public CorsWebFilter corsWebFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true); // Permitir envio de cookies (se necessário)
+        config.setAllowedOrigins(Arrays.asList("http://localhost:5173")); // origem do front-end
+        config.setAllowedHeaders(Arrays.asList("*")); // ou especifique "Authorization", "Content-Type"...
+        config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+
+        return new CorsWebFilter(source);
     }
 }
